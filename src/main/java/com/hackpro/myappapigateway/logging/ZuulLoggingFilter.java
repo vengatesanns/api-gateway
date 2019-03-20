39ee08d41d0de7d2d96d3dc26929c06dc9770b48
vengatesanns <vengat19amd@gmail.com>
2019-03-20 12:13:49 +0530

    Initial Commit for api-gateway

---
+++ src/main/java/com/hackpro/myappapigateway/logging/ZuulLoggingFilter.java
@@ -0,0 +1,42 @@
+package com.hackpro.myappapigateway.logging;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+	
+	private final static Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest httpRequest = RequestContext.getCurrentContext().getRequest();
+		logger.info("RequestUri -> {} , Request -> {}  ", httpRequest.getRequestURI(),httpRequest);
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+	
+	
+
+}
